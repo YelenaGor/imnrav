3f76acf85c14c710f2996214b6237d0e72dbc727
Yelenagor <yelena.goryunova@gmail.com>
2018-06-25 23:54:58 -0400

    Showing the image file

--- fileReaders/image_vectors.py
+++ fileReaders/image_vectors.py
@@ -2,10 +2,17 @@
 import pandas as pd
 import os
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 ## Directories, locations, etc
 imgDir = r"D:\Art\jpg"
 
 ## grab the image locations
-images = os.listdir(imgDir)
+images = [os.path.abspath(os.path.join(imgDir, x)) for x in os.listdir(imgDir)]
 
 print(images[0])
+
+img=mpimg.imread(images[0])
+imgplot = plt.imshow(img)
+plt.show()

96824d17610c75b67a9dfbc92f3c0103cc9f2756
Yelenagor <yelena.goryunova@gmail.com>
2018-06-26 00:09:24 -0400

    Reorganized imports in image_vectors

--- fileReaders/image_vectors.py
+++ fileReaders/image_vectors.py
@@ -1,9 +1,9 @@
 ## Import block
-import pandas as pd
 import os
 
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import pandas as pd
 
 ## Directories, locations, etc
 imgDir = r"D:\Art\jpg"
@@ -13,6 +13,6 @@ images = [os.path.abspath(os.path.join(imgDir, x)) for x in os.listdir(imgDir)]
 
 print(images[0])
 
-img=mpimg.imread(images[0])
+img = mpimg.imread(images[0])
 imgplot = plt.imshow(img)
 plt.show()
